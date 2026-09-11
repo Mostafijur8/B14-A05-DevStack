e7f1bf9ed1d96be6b0364f496f6f32c4c77ce3c2
Mostafijur <mostafijur846661@gmail.com>
2026-09-11 23:27:36 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Nav from "./component/Nav";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Nav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import logo from "../assets/images/logo-text.png";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <img src={logo} alt="logo" />
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
